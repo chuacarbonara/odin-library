35cad8b299448104dd7b53d815e100b1ba8fa4cf
Elisha Chua <218030632+chuacarbonara@users.noreply.github.com>
2025-09-09 21:53:00 +0800

    Make whole body into grid, Add pop-up form on sidebar

---
+++ script.js
@@ -0,0 +1,24 @@
+const myLibrary = [];
+let newBookForm = document.querySelector(".new-book-form");
+let newBookBtn = document.querySelector(".new-book");
+
+function openForm() {
+    document.getElementById("form-container").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("form-container").style.display = "none";
+}
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+newBookBtn.addEventListener("click", () => {
+
+})
+
