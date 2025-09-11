0deea9a7c49ce1541acc0e339165b28cb5e135ce
Elisha Chua <218030632+chuacarbonara@users.noreply.github.com>
2025-09-12 00:05:56 +0800

    Add function to create a div for every accomplished form, Add style to the generated divs

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+const cardLibrary = document.getElementById("library");
+
 function openForm() {
     document.getElementById("form-container").style.display = "block";
 }
@@ -14,16 +16,35 @@ function Book(title, author, genre) {
   this.genre = genre;
 }
 
+function showBook() {
+    const removePrevious = document.querySelectorAll(".card");
+        for (let i = 0; i < removePrevious.length; i++){
+            removePrevious[i].remove();
+        }
+        
+    myLibrary.forEach(myLibrary => {
+        const newCard = document.createElement("div");
+        newCard.classList.add("card")
+        cardLibrary.appendChild(newCard);
+        for (let key in myLibrary) {
+            console.log(`${key}: ${myLibrary[key]}`);
+            const para = document.createElement("p");
+            para.textContent = (`${key}: ${myLibrary[key]}`);
+            newCard.appendChild(para);
+        }
+    })
+}
+
 function addBookToLibrary() {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let genre = document.getElementById("genre").value;
   let newBook = new Book(title, author, genre);
   myLibrary.push(newBook);
-  console.log(myLibrary);
+  showBook();
 }
 
 document.getElementById("new-book-form").addEventListener("submit", (event) => {
     event.preventDefault();
     addBookToLibrary();
-})
+});
