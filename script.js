7beeab7d9c774a25802e00302b4d5c7c97471980
Elisha Chua <218030632+chuacarbonara@users.noreply.github.com>
2025-09-15 21:40:56 +0800

    Add function to remove individual book objects

--- script.js
+++ script.js
@@ -10,10 +10,12 @@ function closeForm() {
     document.getElementById("form-container").style.display = "none";
 }
 
-function Book(title, author, genre) {
+function Book(title, author, genre, reads, id) {
   this.title = title;
   this.author = author;
   this.genre = genre;
+  this.reads = reads;
+  this.id = id;
 }
 
 function showBook() {
@@ -22,29 +24,61 @@ function showBook() {
             removePrevious[i].remove();
         }
         
-    myLibrary.forEach(myLibrary => {
+    myLibrary.forEach(book => {
         const newCard = document.createElement("div");
-        newCard.classList.add("card")
+        newCard.classList.add("card");
+        newCard.dataset.bookId = book.id;
         cardLibrary.appendChild(newCard);
-        for (let key in myLibrary) {
-            console.log(`${key}: ${myLibrary[key]}`);
-            const para = document.createElement("p");
-            para.textContent = (`${key}: ${myLibrary[key]}`);
-            newCard.appendChild(para);
+       
+        const bookTitle = document.createElement("p");
+        bookTitle.innerText = `${book.title}`;
+        bookTitle.style = "width: 100%; background-color: rgba(194, 0, 65, 0.8); font-size: 1.5em; font-weight: bold; text-align: center;"
+        newCard.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement("p");
+        bookAuthor.innerText = `Author: ${book.author}`
+        newCard.appendChild(bookAuthor);
+
+        const bookGenre = document.createElement("p");
+        bookGenre.innerText = `Genre: ${book.genre}`
+        newCard.appendChild(bookGenre);
+
+        const readCheck = document.getElementById("read-status");
+        const readStatus = document.createElement("div");
+        newCard.appendChild(readStatus);
+        if (readCheck.checked == true) {
+            readStatus.innerText = "✓ Read";
+        } else {
+            readStatus.innerText = "x Unread"
         }
-    })
+
+        const removeBtn = document.createElement("button");
+        removeBtn.classList.add("rmvbtn");
+        newCard.appendChild(removeBtn);
+        removeBtn.innerText = "X Remove";
+        removeBtn.addEventListener("click", () => {
+            const bIndex = myLibrary.findIndex(b => b.id === book.id);
+            myLibrary.splice(bIndex, 1);
+            showBook();
+        });
+    });
 }
 
 function addBookToLibrary() {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let genre = document.getElementById("genre").value;
-  let newBook = new Book(title, author, genre);
+  let reads = "Unread";
+  let id = crypto.randomUUID();
+  let newBook = new Book(title, author, genre, reads, id);
   myLibrary.push(newBook);
+  console.log(myLibrary);
   showBook();
 }
 
-document.getElementById("new-book-form").addEventListener("submit", (event) => {
+const bookForm = document.getElementById("new-book-form");
+
+bookForm.addEventListener("submit", (event) => {
     event.preventDefault();
     addBookToLibrary();
 });
